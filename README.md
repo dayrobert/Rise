ca887c3bc670710b0e9511a58914de1b53176685
Robert Day <day.c.robert@gmail.com>
2017-03-07 14:37:58 -0500

    Add readme details and reorg folder structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Rise
 Routines to manage the height of DIV element within a DOM page.  Think Bootstrap grid for the vertical.
+
+A common issue with web layout is the need to control the height of elements within their parent elements.  Rise is a lightweight jQuery extension that allows for the defining the height of a DIV in respect to its parent DIV by assigning rise class values.
+
+By assinging one of the following class definitions you can control the layout height of DIVs within each DIV's parent.
+    rs-fixed:   Accept the rendered height of the current DIV and subtrack this height from all sibling DIVs.
+    rs-full:    Raise the current DIV to the full height of the parent DIV expect for any fixed size DIVs.
+    rs-break:   Raise the current DIV, but do not raise the children of this DIV
+
--- dist/rise.js
+++ dist/rise.js
@@ -2,40 +2,25 @@ var rise = rise || {};
 // Resize the the target value to a hard value and return it's content height.
 // Set the last argument (optional) to true to log the measurements to the JS console
 function setTargetHeight($target, wrapperHt, offset, debug) {
-	var logChanges = (debug !== undefined) && (debug === true);
-	var _containerId = "", _containerHeight = 0;
+	let containerId = "",
 
-	//Figure out the decoration height
-	var _marginHt = $target.outerHeight(true) - $target.outerHeight();
-	var _paddingHt = $target.innerHeight() - $target.height();
-	var _borderHt = $target.outerHeight() - $target.innerHeight();
+	    //Figure out the decoration height
+	    marginHt = $target.outerHeight(true) - $target.outerHeight(),
+	    paddingHt = $target.innerHeight() - $target.height(),
+	    borderHt = $target.outerHeight() - $target.innerHeight();
 
 	//Figure out what the target height should be
-	var _targetHeight = Math.floor(wrapperHt - offset - _marginHt - _paddingHt - _borderHt);
+	var targetHeight = Math.floor(wrapperHt - offset - marginHt - paddingHt - borderHt);
 
 	//Set the outer height of the target
-	$target.height(_targetHeight);
+	$target.height(targetHeight);
 
-	//Log the changes if desired
-	if (logChanges) {
-		_containerId = $target.parent().attr("id");
-		if (!(window.console === undefined)) {
-			console.log(
-				  "-------------------------------------------------\n" +
-				  "Resizing Container\n" +
-				  "  Container:            " + rstrPad(_containerId + ", ", 12, " ") + "\tHeight: " + wrapperHt + "\n" +
-				  "  Target:               " + rstrPad($target.attr("id") + ", ", 12, " ") + "\tOffset: " + offset.toString() + "\tMargin: " + _marginHt + "\tBorder: " + _borderHt + "\tPadding: " + _paddingHt + "\n" +
-				  "  Result Height:        \t\tOuter: " + (_targetHeight + _marginHt + _borderHt + _paddingHt).toString() + "\tInner: " + _targetHeight + "\n\n"
-				  );
-		}
-	}
-
-	return _targetHeight;
+	return targetHeight;
 }
 
 rise.apply = function rise(selector, parentHeight) {
-    var $this = $(selector);
-    var ht = 0;
+    var $this = $(selector),
+        ht = 0;
 
     if (undefined === $this) {
         return;
@@ -74,7 +59,7 @@ rise.apply = function rise(selector, parentHeight) {
         //	Add up the combined height of all the fixed divs
         $divs = $("> div.rs-fixed:visible, >form.rs-fixed:visible, >iframe.rs-fixed:visible", $this);
         var fixedHeight = 0;
-        for (var i = 0, len = $divs.length; i < len; ++i) {
+        for (let i = 0, len = $divs.length; i < len; ++i) {
             $($divs[i]).addClass('rs-risen rs-risen-fixed');
             $("div:visible", $divs[i]).addClass('rs-risen rs-risen-default');
             fixedHeight += $($divs[i]).outerHeight(true);
@@ -82,7 +67,7 @@ rise.apply = function rise(selector, parentHeight) {
 
         // set the height of any full size columns to the height of the parent - the height of the fixed divs
         $divs = $("> div.rs-full:visible, > form.rs-full:visible, > iframe.rs-full:visible", $this);
-        for (var i = 0, len = $divs.length; i < len; ++i) {
+        for (let i = 0, len = $divs.length; i < len; ++i) {
             var $curDiv = $($divs[i]);
             ht = setTargetHeight($curDiv, parentHeight, fixedHeight);
             $curDiv.trigger("risen");
@@ -93,11 +78,11 @@ rise.apply = function rise(selector, parentHeight) {
         //	calc the height of each of the sizeable divs as (height of current div)-(height of all fixed divs) / (number of unfixed divs)
         $divs = $("> div:visible, > form:visible, > iframe:visible", $this).not(".rs-fixed, .rs-full, .rs-3");
         if ($divs.length > 0) {
-            var growHeight = Math.floor((parentHeight - fixedHeight) / $divs.length);
+            let growHeight = Math.floor((parentHeight - fixedHeight) / $divs.length);
 
             //	for each sizeable div set the new height and then rise its child divs
-            for (var i = 0, len = $divs.length; i < len; ++i) {
-                var $curDiv = $($divs[i]);
+            for (let i = 0, len = $divs.length; i < len; ++i) {
+                let $curDiv = $($divs[i]);
                 ht = setTargetHeight($curDiv, growHeight, 0);
                 $curDiv.trigger("risen");
                 $curDiv.addClass('rs-risen rs-risen-calc');
@@ -107,8 +92,10 @@ rise.apply = function rise(selector, parentHeight) {
     }
 };
 
-rise.resizeContent = function resizeContent(elem) {
-    let p = $(elem).parent();
-    let height = p.height();
-    rise.apply(elem, height);
+rise.autoSize = function (elem) {
+    window.onresize( () => {
+        let p = $(elem).parent();
+        let height = p.height();
+        rise.apply(elem, height);
+    });
 };
